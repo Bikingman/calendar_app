d9d07ba9a4ea14266ea660b5f12e98313dc13f65
Bikingman <daniel.patterson87@gmail.com>
2020-08-17 22:21:12 -0400

    for host

--- django/src/calender_app/calender_app/settings.py
+++ django/src/calender_app/calender_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os as os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -122,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
+
+django_heroku.settings(locals())
